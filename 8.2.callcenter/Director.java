0e764e59784855cdfecda0db30d2500f6d2dbfac
avocadoccme <yiransh@gmail.com>
2014-11-28 16:03:50 -0800

    11-28-2014 sd

--- 8.2.callcenter/Director.java
+++
@@ -1,7 +0,0 @@
-package 8.2.callcenter;
-
-class Director extends Employee{
-    public Director(){
-        rank = Rank.Director;
-    }
-}
--- 8.2.callcenter/Manager.java
+++
@@ -1,7 +0,0 @@
-package 8.2.callcenter;
-
-class Manager extends Employee{
-    public Manager(){
-        rank = Rank.Manager;
-    }
-}
